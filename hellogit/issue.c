ded1cac9846391406fd7812280892ab49faf7832
Kaustubh Duraphe <kaustubhduraphe@gmail.com>
2015-09-16 21:13:27 -0700

    Added recommender systems project

---
+++ hellogit/issue.c
@@ -0,0 +1,10 @@
+if(my_name == 'kaustubh')
+	printf("hey kaustubh");
+else if(my_name == 'kastubh')
+	printf("You have wrongly typed my name");
+else if(my_name == 'abcd')
+	printf("hey there abcd");
+else if(my_name == "abcd")
+	printf("hey there");
+
+
